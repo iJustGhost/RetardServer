0d1fcf0171d0df6773086be4ff0d5cd28cb4b746
iJustGhost <fryingchicken81@gmail.com>
2025-02-18 20:02:58 +0800

    [MODIFIED] windows launch script in testing

--- start-windows.bat
+++ start-windows.bat
@@ -1,10 +1,18 @@
+git pull
+
 if exist lock (
     echo "Someone else is running the server! Exiting..."
     exit
 )
 
-git pull
-java -jar paper.jar -nogui
+echo. "">lock
 git add .
-git commit -m "[SERVER SAVED]"
+git commit -m "[SERVER STARTED]"
+git push
+
+java -jar paper.jar -nogui
+
+del "lock"
+git add .
+git commit -m "[SERVER STOPPED]"
 git push
